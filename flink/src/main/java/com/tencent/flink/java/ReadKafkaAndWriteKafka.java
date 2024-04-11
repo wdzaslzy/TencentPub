9b7ee2815e23ecea5e61e4aef2778294bc0eca24
rodenli <rodenli@tencent.com>
2024-04-11 10:33:17 +0800

    add read and write kafka

--- flink/src/main/java/com/tencent/flink/java/ReadKafkaAndWriteKafka.java
+++ flink/src/main/java/com/tencent/flink/java/ReadKafkaAndWriteKafka.java
@@ -1,7 +1,51 @@
 package com.tencent.flink.java;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class ReadKafkaAndWriteKafka {
 
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+                .setBootstrapServers("")
+                .setTopics("lizy_test_topic")
+                .setGroupId("lizy_consumer_group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema()).build();
+
+        KafkaSink<String> kafkaSink = KafkaSink.<String>builder()
+                .setBootstrapServers("")
+                .setRecordSerializer(
+                        KafkaRecordSerializationSchema.builder()
+                                .setTopic("topic-name")
+                                .setValueSerializationSchema(new SimpleStringSchema())
+                                .build()
+                )
+                .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .build();
+
+        DataStreamSource<String> kafkaSourceStream = env.fromSource(kafkaSource,
+                WatermarkStrategy.noWatermarks(), "Kafka_Source");
+
+        kafkaSourceStream.map(new MapFunction<String, String>() {
+            @Override
+            public String map(String value) throws Exception {
+                String[] splits = value.split("_");
+                return splits[0] + "_" + Integer.parseInt(splits[1]) * 2;
+            }
+        }).sinkTo(kafkaSink);
+
+        env.execute("KafkaDemo");
+    }
 
 }
