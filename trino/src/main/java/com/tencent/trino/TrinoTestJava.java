97e1a5727c7069ac67e67d096c9c2d68ceb75b46
rodenli <rodenli@tencent.com>
2024-08-09 17:37:43 +0800

    submit

--- trino/src/main/java/com/tencent/trino/TrinoTestJava.java
+++ trino/src/main/java/com/tencent/trino/TrinoTestJava.java
@@ -10,16 +10,16 @@ import java.util.Properties;
 public class TrinoTestJava {
 
     public static void main(String[] args) {
-        String url = "jdbc:trino://42.194.adsf.31:8443/hive/default";
+        String url = "jdbc:trino://" + args[0] + ":8443/hive/default";
 
         try {
             Properties properties = new Properties();
-            properties.setProperty("user", "root");
-            properties.setProperty("password", "dsf@");
+            properties.setProperty("user", args[1]);
+            properties.setProperty("password", args[2]);
             properties.setProperty("SSL", "true");
             properties.setProperty("SSLKeyStorePath",
-                "/Users/rodenli/Workspace/trino_keystore.jks");
-            properties.setProperty("SSLKeyStorePassword", "trino@123");
+                args[3]);
+            properties.setProperty("SSLKeyStorePassword", args[4]);
             Connection connection = DriverManager.getConnection(url, properties);
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery("show tables");
